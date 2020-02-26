f8e926cb0baf1a5c0425618e21fec08ad616adbc
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-26 17:34:59 +0100

    added id for message and test goes green

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,13 @@ class App extends Component {
             >
               Login
             </button>
-            <p>{message}</p>
+            <p id="message">{message}</p>
           </>
         );
         break;
       case authenticated:
         renderLogin = (
-          <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+          <p id="message">Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
         );
         break;
     }
