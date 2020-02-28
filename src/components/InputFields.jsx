c0c7d74a0b1307b2dc3aa48b69c08c5079f36611
Blake <blake@futchi.com>
2020-02-28 15:16:42 +0100

    turned input field into header element and updated styling

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -1,38 +1,29 @@
 import React from "react";
+import { Box, Header, Grommet, Form, FormField } from "grommet";
+import { grommet } from "grommet/themes";
 
 const InputFields = ({ onChangeHandler }) => {
   return (
     <>
-      <label>Distance</label>
-      <input onChange={onChangeHandler} name="distance" id="distance"></input>
-
-      <select onChange={onChangeHandler} name="gender" id="gender">
-        <option value="female">Female</option>
-        <option value="male">Male</option>
-      </select>
-
-      <label>Age</label>
-      <input onChange={onChangeHandler} name="age" id="age"></input>
+      <Grommet theme={grommet}>
+        <Header background="light-4" pad="small">
+          <Box direction="column" gap="medium">
+          <Form align>
+        <FormField onChange={onChangeHandler} id ="distance" name="distance" placeholder="Distance"></FormField>
+        <FormField onChange={onChangeHandler} id ="age" name="age" placeholder="Age"></FormField>
+        <FormField htmlFor="select"> 
+        <select onChange={onChangeHandler} name="gender" id="gender">
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+        </FormField>
+        </Form>
+          </Box>
+        </Header>
+      </Grommet>
     </>
   );
 };
 
 export default InputFields;
 
-
-
-
-{/* <Grommet full theme={grommet}>
-<Box fill align="center" justify="center">
-  <Box width="medium">
-    <Form>
-    <FormField onChange={onChangeHandler} id ="distance" name="distance" placeholder="Distance"></FormField>
-    <FormField htmlFor="select"> 
-      <Select onChange={onChangeHandler} htmlFor="select" options={'male', 'female'} name="gender" id="gender">
-      </Select>
-    </FormField>
-    <FormField onChange={onChangeHandler} id ="age" name="age" placeholder="Age"></FormField>
-    </Form>
-  </Box>
-</Box>
-</Grommet> */}
