870f78174c15f210a5d2c00b8c7d839b7d57b130
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-29 12:32:53 +0100

    fixed typo updated register.js

--- cypress/integration/UserCanRegister.feature.js
+++ cypress/integration/UserCanRegister.feature.js
@@ -25,22 +25,4 @@ describe("User can register", () => {
       cy.get("#message").should("contain", "Hi user@mail.com");
     });
   
-    // it("with invalid credentials", () => {
-    //   cy.route({
-    //     method: "POST",
-    //     url: "http://localhost:3000/api/v1/auth/",
-    //     status: "401",
-    //     response: {
-    //       errors: ["Invalid login credentials. Please try again."],
-    //       success: false
-    //     }
-    //   });
-    //   cy.get("#login").click();
-    //   cy.get("#login-form").within(() => {
-    //     cy.get("#email").type("user@mail.com");
-    //     cy.get("#password").type("wrongpassword");
-    //     cy.get('button').contains('Submit').click()
-    //   });
-    //   cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
-    // });
   });
--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import InputFields from "./components/InputFields";
 import LoginForm from "./components/LoginForm";
 import { authenticate } from "./modules/auth";
 import DisplayPerformanceData from "./components/DisplayPerformanceData";
-import { grommet, Grommet, Box, Button } from 'grommet';
+import { grommet, Grommet, Box, Button } from "grommet";
 import RegisterForm from "./components/RegisterForm";
-import { register } from "./index/register";
-
+import { register } from "./modules/register";
 
 class App extends Component {
   state = {
@@ -17,29 +16,47 @@ class App extends Component {
     renderLoginForm: false,
     authenticated: false,
     message: "",
-    entrySaved: false
+    entrySaved: false,
+    renderRegisterForm: false,
+    registred: false
   };
 
   onChangeHandler = e => {
     this.setState({ [e.target.name]: e.target.value, entrySaved: false });
   };
   render() {
-    const { renderLoginForm, authenticated, message } = this.state;
+    const {
+      renderLoginForm,
+      authenticated,
+      message,
+      renderRegisterForm
+    } = this.state;
     let renderLogin;
     let performanceDataIndex;
+    let renderRegister
+
     switch (true) {
       case renderLoginForm && !authenticated:
         renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case renderRegisterForm && !authenticated:
+        renderRegister = <RegisterForm submitFormHandler={this.onRegister} />;
         break;
       case !renderLoginForm && !authenticated:
         renderLogin = (
           <>
             <Grommet theme={grommet}>
               <Box align="center" pad="medium">
-                <Button 
-                label="Login" 
-                id="login" 
-                onClick={() => this.setState({ renderLoginForm: true })} />
+                <Button
+                  label="Login"
+                  id="login"
+                  onClick={() => this.setState({ renderLoginForm: true })}
+                />
+                <Button
+                    label="Register"
+                    id="register"
+                    onClick={() => this.setState({ renderRegisterForm: true })}
+                  />
               </Box>
               <Box align="center" pad="medium">
                 <p id="message">{message}</p>
@@ -48,51 +65,82 @@ class App extends Component {
           </>
         );
         break;
+        // case !renderRegisterForm && !authenticated:
+        //   renderRegister = (
+        //     <>
+        //       <Grommet theme={grommet}>
+        //         <Box align="center" pad="medium">
+        //           <Button
+        //             label="Register"
+        //             id="register"
+        //             onClick={() => this.setState({ renderRegisterForm: true })}
+        //           />
+        //         </Box>
+        //         <Box align="center" pad="medium">
+        //           <p id="message">{message}</p>
+        //         </Box>
+        //       </Grommet>
+        //     </>
+        //   );
+        //   break;
       case authenticated:
         renderLogin = (
-          <p id="message">Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+          <p id="message">
+            Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}
+          </p>
         );
         performanceDataIndex = (
-          <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
+          <button
+            id="show-index"
+            onClick={() => this.setState({ renderIndex: true })}
+          >
+            Show past entries
+          </button>
         );
         if (this.state.renderIndex) {
           performanceDataIndex = (
             <>
-              <DisplayPerformanceData 
+              <DisplayPerformanceData
                 updateIndex={this.state.updateIndex}
                 indexUpdated={() => this.setState({ updateIndex: false })}
               />
-              <button onClick={() => this.setState({ renderIndex: false })}>Hide past entries</button>
+              <button onClick={() => this.setState({ renderIndex: false })}>
+                Hide past entries
+              </button>
             </>
-          )
+          );
         } else {
           performanceDataIndex = (
-            <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
-          )
+            <button
+              id="show-index"
+              onClick={() => this.setState({ renderIndex: true })}
+            >
+              Show past entries
+            </button>
+          );
         }
-      
+
         break;
-        
     }
 
     return (
-    
       <>
-      <Grommet className="App">
-      <InputFields onChangeHandler={this.onChangeHandler} />
-        {renderLogin}
-        {renderRegister}
-        <DisplayCooperResult
-          distance={this.state.distance}
-          gender={this.state.gender}
-          age={this.state.age}
-          authenticated={this.state.authenticated}
-          entrySaved={this.state.entrySaved}
-          entryHandler={() => this.setState({ entrySaved: true, updateIndex: true })}
-        />
-        {performanceDataIndex}
-      </Grommet>
-        
+        <Grommet className="App">
+          <InputFields onChangeHandler={this.onChangeHandler} />
+          {renderLogin}
+          {renderRegister}
+          <DisplayCooperResult
+            distance={this.state.distance}
+            gender={this.state.gender}
+            age={this.state.age}
+            authenticated={this.state.authenticated}
+            entrySaved={this.state.entrySaved}
+            entryHandler={() =>
+              this.setState({ entrySaved: true, updateIndex: true })
+            }
+          />
+          {performanceDataIndex}
+        </Grommet>
       </>
     );
   }
@@ -110,7 +158,7 @@ class App extends Component {
   };
   onRegister = async e => {
     e.preventDefault();
-    const response = await authenticate(
+    const response = await register(
       e.target.email.value,
       e.target.password.value,
       e.target.password_confirmation.value
